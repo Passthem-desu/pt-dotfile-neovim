0dca28d65c8e45fd756ee6c45f2bdefd9ba273f5
Passthem <passthem183@gmail.com>
2026-03-04 10:29:56 +0800

    添加 Typescript 相关的 Language Server

--- lua/passthem/plugins/lsp/mason.lua
+++ lua/passthem/plugins/lsp/mason.lua
@@ -1,60 +1,61 @@
 return {
-    "mason-org/mason.nvim",
-    lazy = false,
-    dependencies = {
-        "mason-org/mason-lspconfig.nvim",
-        "WhoIsSethDaniel/mason-tool-installer.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "neovim/nvim-lspconfig",
-        "saghen/blink.cmp",
-    },
-    config = function()
-        -- import mason and mason_lspconfig
-        local mason = require("mason")
-        local mason_lspconfig = require("mason-lspconfig")
-        local mason_tool_installer = require("mason-tool-installer")
+	"mason-org/mason.nvim",
+	lazy = false,
+	dependencies = {
+		"mason-org/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		"hrsh7th/cmp-nvim-lsp",
+		"neovim/nvim-lspconfig",
+		"saghen/blink.cmp",
+	},
+	config = function()
+		-- import mason and mason_lspconfig
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
 
-        mason.setup()
+		mason.setup()
 
-        mason_lspconfig.setup({
-            ensure_installed = {
-                -- lua
-                "lua_ls",
+		mason_lspconfig.setup({
+			ensure_installed = {
+				-- lua
+				"lua_ls",
 
-                -- python
-                "ruff",
-                "basedpyright",
+				-- python
+				"ruff",
+				"basedpyright",
 
-                -- rust
-                "rust_analyzer",
+				-- rust
+				"rust_analyzer",
 
-                -- golang
-                "gopls",
+				-- golang
+				"gopls",
 
-                -- c/c++
-                "clangd",
+				-- c/c++
+				"clangd",
 
-                -- web
-                "svelte",
-                "tailwindcss",
-                "astro",
-                "eslint",
+				-- web
+				"svelte",
+				"tailwindcss",
+				"astro",
+				"eslint",
+				"vtsls",
 
-                -- misc
-                "just",
-                "dockerls",
-                "docker_compose_language_service",
-                "taplo",  -- toml 支持
-            },
-        })
+				-- misc
+				"just",
+				"dockerls",
+				"docker_compose_language_service",
+				"taplo", -- toml 支持
+			},
+		})
 
-        mason_tool_installer.setup({
-            ensure_installed = {
-                -- web
-                "eslint_d",
-                "prettier",
-                "prettierd",
-            },
-        })
-    end,
+		mason_tool_installer.setup({
+			ensure_installed = {
+				-- web
+				"eslint_d",
+				"prettier",
+				"prettierd",
+			},
+		})
+	end,
 }
