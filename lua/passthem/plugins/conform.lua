1f54fce50be32cf505f24567ddda6d925633afba
passthem <Passthem183@gmail.com>
2026-04-07 03:50:43 +0800

    保证一定用 prettier

--- lua/passthem/plugins/conform.lua
+++ lua/passthem/plugins/conform.lua
@@ -2,15 +2,35 @@ return {
     "stevearc/conform.nvim",
     opts = {
         formatters_by_ft = {
+            -- 基础脚本 (Base Scripts)
             javascript = { "prettier" },
-            javascriptreact = { "prettier" },
             typescript = { "prettier" },
+            javascriptreact = { "prettier" },
             typescriptreact = { "prettier" },
-            ["javascript.jsx"] = { "prettier" },
+
+            -- 框架组件 (Framework Components)
+            vue = { "prettier" },
+            svelte = { "prettier" },
+            astro = { "prettier" },
+
+            -- 样式表 (Stylesheets)
+            css = { "prettier" },
+            scss = { "prettier" },
+            less = { "prettier" },
+            postcss = { "prettier" },
+
+            -- 标记语言与配置 (Markup & Configs)
+            html = { "prettier" },
+            json = { "prettier" },
+            jsonc = { "prettier" }, -- 带注释的 JSON (JSON with Comments)
+            yaml = { "prettier" },
+            markdown = { "prettier" },
+            ["markdown.mdx"] = { "prettier" },
+            graphql = { "prettier" },
         },
         format_on_save = {
             timeout_ms = 500,
-            lsp_fallback = true,
+            lsp_fallback = false, -- 强制不使用 LSP，只信赖 Prettier
         },
     },
 }
