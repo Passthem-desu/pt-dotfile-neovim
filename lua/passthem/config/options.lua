b4d84133fddbcac61d14a3705a48878be342b653
passthem <Passthem183@gmail.com>
2026-04-27 23:31:14 +0800

    禁用 OSC 52 的粘贴

--- lua/passthem/config/options.lua
+++ lua/passthem/config/options.lua
@@ -3,7 +3,17 @@
 vim.opt.clipboard = "unnamedplus"
 
 -- 见 `:help clipboard-osc52`
-vim.g.clipboard = 'osc52'
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  -- paste = {
+  --   ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+  --   ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  -- },
+}
 
 -- 支持鼠标操作
 vim.opt.mouse = "a"
