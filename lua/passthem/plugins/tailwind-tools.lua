5c2de231bbc070929d970007ffadfb93881cf0ac
passthem <Passthem183@gmail.com>
2026-08-25 20:03:00 +0800

    禁用 Tailwind Tools，我不太需要那种颜色显示了

--- lua/passthem/plugins/tailwind-tools.lua
+++ lua/passthem/plugins/tailwind-tools.lua
@@ -1,28 +1,30 @@
 -- 可以在前端项目使用的，查看各种颜色值的颜色
 
-return {
-    "roobert/tailwindcss-colorizer-cmp.nvim",
-    {
-        "NvChad/nvim-colorizer.lua",
-        dependencies = { "nvim-treesitter/nvim-treesitter" },
-        opts = {},
-        config = function()
-            local nvchadcolorizer = require("colorizer")
-            local tailwindcolorizer = require("tailwindcss-colorizer-cmp")
+-- return {
+--     "roobert/tailwindcss-colorizer-cmp.nvim",
+--     {
+--         "NvChad/nvim-colorizer.lua",
+--         dependencies = { "nvim-treesitter/nvim-treesitter" },
+--         opts = {},
+--         config = function()
+--             local nvchadcolorizer = require("colorizer")
+--             local tailwindcolorizer = require("tailwindcss-colorizer-cmp")
+--
+--             nvchadcolorizer.setup({
+--                 options = {
+--                     parsers = {
+--                         css = true,
+--                         tailwind = { enable = true },
+--                     },
+--                 },
+--             })
+--
+--             tailwindcolorizer.setup({
+--                 color_square_width = 2,
+--             })
+--
+--         end,
+--     },
+-- }
 
-            nvchadcolorizer.setup({
-                options = {
-                    parsers = {
-                        css = true,
-                        tailwind = { enable = true },
-                    },
-                },
-            })
-
-            tailwindcolorizer.setup({
-                color_square_width = 2,
-            })
-
-        end,
-    },
-}
+return {}
